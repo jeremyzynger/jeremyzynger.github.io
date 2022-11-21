aeb9f0a6e46baf2f40794b06891a77602f495eb7
abj95630 <abah20000@gmail.com>
2022-11-21 19:06:51 +0100

    Ajout de 3 informations supplémentaires
    Affichage de la ville, de la température minimale et maximale.

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-let meteo = document.getElementById("meteo");
-
-fetch("https://api.meteo-concept.com/api/forecast/daily/0?token=74b90bd6180f93281bc8096d7f2f512765009b478573094d89c7d54eea11ca55&insee=69123")
+fetch("https://api.meteo-concept.com/api/forecast/daily/0?token=74b90bd6180f93281bc8096d7f2f512765009b478573094d89c7d54eea11ca55&insee=2A041")
 .then((response) => {
     if(!response.ok) {
         throw Error(response.statusText);
@@ -9,6 +7,8 @@ fetch("https://api.meteo-concept.com/api/forecast/daily/0?token=74b90bd6180f9328
 })
 .then((data) => {
     console.log(data)
-    meteo.innerHTML =  "Température maximal à Lyon : " + JSON.stringify(data.forecast.tmax) + "°C";
+    document.getElementById("ville").innerHTML = "Ville : " + data.city.name;
+    document.getElementById("meteo-min").innerHTML =  "Température minimal : " + JSON.stringify(data.forecast.tmin) + "°C";
+    document.getElementById("meteo-max").innerHTML =  "Température maximal : " + JSON.stringify(data.forecast.tmax) + "°C";
 })
 .catch(error => alert("Erreur : " + error))
