9bf6ad566ea00bfe90aad446f528d79486dbb299
mimi <jeremyzynger@gmail.com>
2022-11-22 13:19:57 +0100

    V1

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 fetch(api())
-    .then((response) => {
-        if(!response.ok) {
-            throw Error(response.statusText);
-        }
-        return response.json();
-    })
-    .then((data) => {
-        console.log(data)
-        document.getElementById("ville").innerHTML = "Ville : " + data.city.name;
-        document.getElementById("meteo-min").innerHTML =  "Température minimal : " + JSON.stringify(data.forecast.tmin) + "°C";
-        document.getElementById("meteo-max").innerHTML =  "Température maximal : " + JSON.stringify(data.forecast.tmax) + "°C";
-    })
-    .catch(error => alert("Erreur : " + error))
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    document.getElementById("ville").innerHTML = "Ville : " + data.city.name;
+    document.getElementById("meteo-min").innerHTML =
+      "Température minimal : " + JSON.stringify(data.forecast.tmin) + "°C";
+    document.getElementById("meteo-max").innerHTML =
+      "Température maximal : " + JSON.stringify(data.forecast.tmax) + "°C";
+  })
+  .catch((error) => alert("Erreur : " + error));
+
+function runClock() {
+  var today = new Date();
+  var hours = today.getHours();
+  var minutes = today.getMinutes();
+  var seconds = today.getSeconds();
+  var timeValue = hours;
+
+  timeValue += (minutes < 10 ? ":0" : ":") + minutes;
+  timeValue += (seconds < 10 ? ":0" : ":") + seconds;
+  document.getElementById("heure").innerHTML = timeValue;
+  console.log(timeValue);
+}
+runClock();
+setInterval(runClock, 1000);
