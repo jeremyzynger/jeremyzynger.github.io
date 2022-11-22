fffdd2e729c30cb6e995e6e8839300dd6982a5b6
abj95630 <abah20000@gmail.com>
2022-11-22 11:12:51 +0100

    Revert "Création du fichier de configuration"
    This reverts commit 782a7ebaf493c35ca27bd5846861c725337d75a9.

--- configuration.js
+++
@@ -1,4 +0,0 @@
-function api() {
-    let api = "https://api.meteo-concept.com/api/forecast/daily/0?token=74b90bd6180f93281bc8096d7f2f512765009b478573094d89c7d54eea11ca55&insee=67267"
-    return api
-}
