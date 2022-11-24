81f8f61b86736b304b152c34cedfdba8c8ba72b3
mimi <jeremyzynger@gmail.com>
2022-11-24 15:41:34 +0100

    fixsun1

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-fetch(api())
-  .then((response) => {
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-    document.getElementById("ville").innerHTML = "Ville : " + data.city.name;
-    document.getElementById("meteo-min").innerHTML =
-      "Température ambiante : " +
-      JSON.stringify(data.forecast[0].temp2m) +
-      "°C";
-  })
-  .catch((error) => alert("Erreur : " + error));
+// fetch(api())
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw Error(response.statusText);
+//     }
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     document.getElementById("ville").innerHTML = "Ville : " + data.city.name;
+//     document.getElementById("meteo-min").innerHTML =
+//       "Température ambiante : " +
+//       JSON.stringify(data.forecast[0].temp2m) +
+//       "°C";
+//   })
+//   .catch((error) => alert("Erreur : " + error));
 
 let hours = 0;
 
@@ -31,7 +31,38 @@ function runClock() {
 runClock();
 setInterval(runClock, 1000);
 
-if (hours >= 12) {
-  console.log("lol");
-  document.getElementById("sun").style.top = "-30%";
+// if (hours <= 12) {
+//   console.log("lol");
+//   document.getElementById("sun").style.top = "-30%";
+// }
+
+// const nuit =
+//   hours <= 6 || hours >= 18
+//     ? (document.getElementById("sun").style.top = "-30%")
+//     : hours == 7
+//     ? (document.getElementById("sun").style.top = "60%")
+//     : hours == 8
+//     ? (document.getElementById("sun").style.top = "50%")
+//     : hours == 9
+//     ? (document.getElementById("sun").style.top = "40%")
+//     : hours == 10
+//     ? (document.getElementById("sun").style.top = "30%")
+//     : hours == 11
+//     ? (document.getElementById("sun").style.top = "20%")
+//     : hours == 12
+//     ? (document.getElementById("sun").style.top = "10%")
+//     : (document.getElementById("sun").style.top = "70%");
+
+// for (let i = 6; i < 12; i++) {
+//   console.log;
+//   if (i == hours) {
+//     console.log(i);
+//     document.getElementById("sun").style.top += "10%";
+//   }
+// }
+
+function mvtSun(x) {
+  let a = Math.abs(x - 12) * (60 / 8) + 10;
+  document.getElementById("sun").style.top = `${a}%`;
 }
+setInterval(mvtSun(hours), 1000);
