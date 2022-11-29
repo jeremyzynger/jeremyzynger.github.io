4ad9b18a398ecd4abea009d6dd7161595fc36c5f
mimi <jeremyzynger@gmail.com>
2022-11-29 16:13:34 +0100

    hardregroup

--- script.js
+++ script.js
@@ -1,25 +1,67 @@
-fetch(api())
-  .then((response) => {
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-    document.getElementById("ville").innerHTML = "Ville : " + data.city.name;
-    document.getElementById("meteo").innerHTML =
-      "Température ambiante : " +
-      JSON.stringify(data.forecast[0].tsoil1) +
-      "°C";
-    document.getElementById("rain").innerHTML =
-      "Probabilité de pluie : " + data.forecast[0].probarain + "%";
+function fetchApi(codeInsee) {
+  fetch(api(codeInsee))
+    .then((response) => {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      document.getElementById("meteo").innerHTML =
+        Number(data.forecast[0].tsoil1) + "°C";
+      document.getElementById("rain").innerHTML =
+        "Probabilité de pluie : " + data.forecast[0].probarain + "%";
 
-    setCloud(data.forecast[0].probarain);
-    setHeavyCloud(data.forecast[0].probarain);
-    setRain(data.forecast[0].probarain);
-  })
-  .catch((error) => alert("Erreur : " + error));
+      setCloud(data.forecast[0].probarain);
+      setHeavyCloud(data.forecast[0].probarain);
+      setRain(data.forecast[0].probarain);
+    })
+    .catch((error) => alert("Erreur : " + error));
+}
+
+let tabVille = [
+  {
+    ville: "PARIS",
+    code: "75056",
+  },
+  {
+    ville: "LYON",
+    code: "69123",
+  },
+  {
+    ville: "STRASBOURG",
+    code: "67482",
+  },
+  {
+    ville: "ROUEN",
+    code: "76540",
+  },
+  {
+    ville: "MARSEILLE",
+    code: "13055",
+  },
+  {
+    ville: "AJACCIO",
+    code: "2A004",
+  },
+  {
+    ville: "BREST",
+    code: "29019",
+  },
+  {
+    ville: "BIARRITZ",
+    code: "64122",
+  },
+  {
+    ville: "BORDEAUX",
+    code: "33063",
+  },
+  {
+    ville: "NICE",
+    code: "06088",
+  },
+];
 
 function runClock() {
   var today = new Date();
@@ -30,6 +72,30 @@ function runClock() {
   timeValue += (minutes < 10 ? ":0" : ":") + minutes;
   document.getElementById("heure").innerHTML = timeValue;
   console.log(timeValue);
+}
+
+let index = 0;
+let container = document.getElementById("container-ville");
+let p = document.createElement("p");
+p.id = "ville";
+p.innerHTML = tabVille[index].ville;
+container.appendChild(p);
+
+fetchApi(tabVille[index].code);
+
+function changeSlide(sens) {
+  index = index + sens;
+
+  if (index < 0) {
+    index = tabVille.length - 1;
+  }
+  if (index > tabVille.length - 1) {
+    index = 0;
+  }
+
+  fetchApi(tabVille[index].code);
+  p.innerHTML = tabVille[index].ville;
+  return container.appendChild(p);
 }
 
 runClock();
